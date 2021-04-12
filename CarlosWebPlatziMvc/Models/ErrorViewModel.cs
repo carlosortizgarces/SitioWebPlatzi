44784ddff82be081608a2b6cae52f234c23b8439
Carlos Ortiz <carlosortizgarces@outlook.com>
2021-04-11 22:42:34 -0500

    proyecto actualizado

---
+++ CarlosWebPlatziMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarlosWebPlatziMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
